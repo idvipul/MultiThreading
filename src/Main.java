8682be15f2055b4403e8a7f7eb64f366c2898ca4
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-07-18 19:00:51 -0700

    MultiThreading with thread class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        A obj = new A();
+        obj.start(); // starts the thread by calling run method
+
+        B obj1 = new B();
+        obj1.start();
+    }
+}
+
+class A extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
